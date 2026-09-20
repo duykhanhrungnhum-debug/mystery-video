7982f7fc4cd9db1ae54335cd56fd4bc1288563a6
duykhanhrungnhum-debug <duykhanhrungnhum@gmail.com>
2026-09-20 09:50:32 +0700

    test: use non-repetitive healthy story fixture

--- tests/test_story_quality.py
+++ tests/test_story_quality.py
@@ -20,15 +20,26 @@ def test_detects_runaway_repetitive_rewrite():
 
 
 def test_accepts_coherent_rewrite_close_to_source_length():
-    source = (
-        "Lưu Bị gặp Trương Phi tại Trác Quận. Sau đó hai người gặp Quan Vũ. "
-        "Cả ba cùng bàn việc chiêu mộ nghĩa binh để chống quân Khăn Vàng. "
-    ) * 10
-    healthy = (
-        "Tại Trác Quận, Lưu Bị gặp Trương Phi rồi làm quen với Quan Vũ. "
-        "Ba người cùng chí hướng, quyết định chiêu mộ nghĩa binh và đứng lên "
-        "chống quân Khăn Vàng đang gây loạn khắp nơi. "
-    ) * 8
+    source = " ".join([
+        "Lưu Bị gặp Trương Phi tại Trác Quận.",
+        "Hai người sau đó làm quen với Quan Vũ.",
+        "Ba người cùng chí hướng và bàn việc chiêu mộ nghĩa binh.",
+        "Họ chuẩn bị vũ khí và tập hợp những người muốn chống quân Khăn Vàng.",
+        "Lưu Bị nhớ đến chí hướng cứu dân giữa lúc thiên hạ rối ren.",
+        "Trương Phi góp tiền của để giúp việc tuyển quân.",
+        "Quan Vũ đồng ý sát cánh cùng hai người trong các trận chiến sắp tới.",
+        "Cả ba quyết định cùng lên đường sau khi chuẩn bị xong.",
+    ])
+    healthy = " ".join([
+        "Tại Trác Quận, Lưu Bị gặp Trương Phi và nhanh chóng nhận ra họ có cùng chí hướng.",
+        "Không lâu sau, hai người gặp Quan Vũ, một tráng sĩ cũng muốn dẹp loạn cứu dân.",
+        "Ba người bàn bạc rồi quyết định chiêu mộ nghĩa binh để chống quân Khăn Vàng.",
+        "Trương Phi dùng tiền của trong nhà để hỗ trợ việc mua sắm vũ khí và tuyển người.",
+        "Lưu Bị nhắc mọi người rằng mục tiêu của họ là bảo vệ dân chúng giữa thời loạn.",
+        "Quan Vũ nhận lời sát cánh, cam kết cùng hai người vượt qua những trận chiến phía trước.",
+        "Sau khi quân sĩ và lương thảo được chuẩn bị, đội nghĩa binh bắt đầu hình thành.",
+        "Ba người rời Trác Quận với quyết tâm thực hiện chí hướng đã thống nhất.",
+    ])
 
     assert is_degenerate_rewrite(healthy, source) is False
 
