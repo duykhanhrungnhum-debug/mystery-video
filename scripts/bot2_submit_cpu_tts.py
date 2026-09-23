afe8e2377974f46e777ff6de1cb467a06545254d
duykhanhrungnhum-debug <duykhanhrungnhum@gmail.com>
2026-09-23 14:47:59 +0700

    bot2: pin stage checkpoint v2

--- scripts/bot2_submit_cpu_tts.py
+++ scripts/bot2_submit_cpu_tts.py
@@ -9,8 +9,8 @@ from urllib.request import Request, urlopen
 
 from mystery_video.kaggle_submit import KaggleClient
 
-AI_WORKER_REVISION = "65957a117498edeec071a8030cd993d1d3f1056d"
-CHECKPOINT_REVISION = "3bf7f1507b3936b56ded8398b078131d72c0ac46"
+AI_WORKER_REVISION = "dc2e0efad88d567e2d57063836a4f940e23dcfed"
+CHECKPOINT_REVISION = "hb-stage-v2"
 AI_WORKER_URL = f"https://raw.githubusercontent.com/duykhanhrungnhum-debug/AI-/{AI_WORKER_REVISION}/hidden_beyond/longform_audio_worker_v3.py"
 
 
