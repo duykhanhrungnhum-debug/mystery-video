31e68584a72b25834751dfcf0f5b3a50c95e624d
duykhanhrungnhum-debug <duykhanhrungnhum@gmail.com>
2026-09-23 14:56:46 +0700

    bot2: pin verified caption-first stage worker

--- scripts/bot2_submit_gpu.py
+++ scripts/bot2_submit_gpu.py
@@ -9,7 +9,7 @@ from urllib.request import Request, urlopen
 
 from mystery_video.kaggle_submit import KaggleClient
 
-AI_WORKER_REVISION = "1d6855140575f30a84ed19039b4495f57af4142b"
+AI_WORKER_REVISION = "4e64d9f0c5b2723a264f1770c7c0fa9fba900408"
 CHECKPOINT_REVISION = "hb-stage-v2"
 AI_WORKER_URL = f"https://raw.githubusercontent.com/duykhanhrungnhum-debug/AI-/{AI_WORKER_REVISION}/hidden_beyond/longform_audio_worker_v3.py"
 
