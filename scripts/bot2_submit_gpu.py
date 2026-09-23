fa1803b566bce6b46eff6b6d03dd9c5d425c2c72
duykhanhrungnhum-debug <duykhanhrungnhum@gmail.com>
2026-09-23 14:19:53 +0700

    bot2: pin ASR drift checkpoint fix

--- scripts/bot2_submit_gpu.py
+++ scripts/bot2_submit_gpu.py
@@ -9,7 +9,7 @@ from urllib.request import Request, urlopen
 
 from mystery_video.kaggle_submit import KaggleClient
 
-AI_WORKER_REVISION = "ea0ea10b1e1c94e3f66921dc35ac08e615847e32"
+AI_WORKER_REVISION = "ee13877eedaeffaaa256210a2bb0c1e1be2ddc20"
 CHECKPOINT_REVISION = "3bf7f1507b3936b56ded8398b078131d72c0ac46"
 AI_WORKER_URL = f"https://raw.githubusercontent.com/duykhanhrungnhum-debug/AI-/{AI_WORKER_REVISION}/hidden_beyond/longform_audio_worker_v3.py"
 
